9410094ba7be7baf300cd320afb3c43c642e28e2
mokautsar30 <mohammadkautsar30@gmail.com>
2024-05-17 11:10:05 +0700

    feat: commit on second branch test

--- test_1.js
+++ test_1.js
@@ -3,3 +3,18 @@ function sayHello(str) {
 }
 
 console.log(sayHello('mokautsar'));
+
+
+
+function sumWord(str) {
+    let sum = 0
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] !== str.length) {
+            sum++
+        }
+    }
+    return sum
+
+}
+
+console.log(sumWord('mokautsar'));
